5dbe7d38f8a6dbbe38152fc8b8007a9998f5d598
qmsggg <luohao0414@gmail.com>
2019-08-31 18:17:10 -0700

    install

---
+++ t2/src/main.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 31 Aug 2019 05:26:21 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("Hello World from t1 Main!\n");
+    return 0;
+}
