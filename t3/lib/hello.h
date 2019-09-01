96c0c794ff2b07fc4f093086e4f4b211ed9e0760
qmsggg <luohao0414@gmail.com>
2019-08-31 20:13:34 -0700

    add t3

---
+++ t3/lib/hello.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.h
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 31 Aug 2019 07:43:22 PM PDT
+ ************************************************************************/
+
+#ifndef _HELLO_H
+#define _HELLO_H
+
+#include <stdio.h>
+void HelloFunc();
+#endif
